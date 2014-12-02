6613384ee95a4262cae9eebd54a9007784f8bd63
unknown <barta.gergo.bme@gmail.com>
2014-12-02 11:55:04 +0100

    haversine v1

--- bigdataf1.R
+++ bigdataf1.R
@@ -11,9 +11,15 @@ rmr.options(backend="local")
 d0 = read.csv('e:/munka/BME/BigData/siri.20121125.csv',col.names = c('ts','line_id','direction','journey_pattern_id','time_frame','vehicle_journey_id','operator','congestion','lon','lat','delay','block_id','vehicle_id','stop_id','at_stop'))
 #subset(d0[ ! duplicated( d0[ c("line_id","operator") ] ) , ], select=c("line_id","operator"))
 #summary(d0)
+d0$id <- 1:nrow(d0)
+# TODO order by first
 
 # lenyomjuk az adatot "hadoop"-ba
 hd0 <- to.dfs(d0)
+
+#csinalunk egy pici mintat
+td0 <- to.dfs(head(d0,5))
+
 # egyedi line, operator parok kinyerese trukkos aggregacioval TODO: lehetne szebb
 hlines_ops <-  mapreduce(input = hd0, 
                          map = function(., v)
@@ -46,6 +52,44 @@ hdelay_per_line <-  mapreduce(input = hd0,
                                 keyval(k, mean(vv))
                               }
 )
+
+# atlagos keses vonalankent 2. szamu megoldas
+
+hdelay_per_line <- mapreduce(hd0, 
+    map = function(k, v)
+        keyval(v$line_id, v$delay), 
+    reduce = function(k, v) 
+        cbind(line = k, mean = mean(v, na.rm = TRUE)))
+result = from.dfs(hdelay_per_line)
+head(result)
+
+haversines <- mapreduce(td0, 
+  map = function(k, v)
+    keyval(c(v$id,v$id+1),cbind(v$lat,v$lon)),   
+   reduce = function(k, v) {
+     # kinyerjuk a koordinatakat a reduce set-bol
+     lat = v[1]
+     lon = v[3]
+     plat = v[2]
+     plon= v[4] 
+     # radian konverzio
+     rlat = lat*pi/180
+     rlon = lon*pi/180
+     rplat = plat*pi/180
+     rplon= plon*pi/180
+     
+     # haversine tavolsag szamitasa
+     R <- 6371 # Earth mean radius [km]
+     delta.long <- (rplon - rlon)
+     delta.lat <- (rplat - rlat)
+     a <- sin(delta.lat/2)^2 + cos(rlat) * cos(rplat) * sin(delta.long/2)^2
+     c <- 2 * asin(min(1,sqrt(a)))
+     cbind(id = k, lat = lat, lon = lon, plat = plat, plon= plon, distdelta = R*c)})
+   
+result = from.dfs(haversines)$val
+head(result)
+
+#distHaversine(c(53.39514, -6.375028), c(53.376373, -6.587523))
 
 # eredmenyek kiolvasasa
 from.dfs(hdelay_per_line)
