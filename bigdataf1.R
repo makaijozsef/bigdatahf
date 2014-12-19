245305e064e9a6822e3dc347498be0f05aeaeb27
unknown <barta.gergo.bme@gmail.com>
2014-12-19 23:49:44 +0100

    szakaszokra bontas, mapreduce-ok sorszamozasa, tovabbi kommentek

--- bigdataf1.R
+++ bigdataf1.R
@@ -1,4 +1,4 @@
-# IMPORTS
+############### IMPORTS #############################
 library('devtools')
 library('lattice')
 library('ggmap')
@@ -9,31 +9,55 @@ library('rmr2')
 library('bigvis')
 rmr.options(backend="local")
 
+############### SEGEDFUGGVENYEK ###################
+# segedfuggveny hogy belenezhessunk az eredmenyekbe
 rread <- function(dset,tt = TRUE) {
   if (tt) result = from.dfs(dset)$val
   else result = from.dfs(dset)
   return(head(result))  
 }
 
-# Egy adott könyvtár 4-13. (csv) fájljait összefuzzük egy nagy dataframe-be
-# És még hozzáadunk sorfolytonos id és olyan attribútumot timestamp alapján, hogy mely órában történt megfigyelés
+# NA-val tuzdelt ertekek osszevetese
+compareNA <- function(v1,v2) {
+  # This function returns TRUE wherever elements are the same, including NA's,
+  # and false everywhere else.
+  same <- (v1 == v2)  |  (is.na(v1) & is.na(v2))
+  same[is.na(same)] <- FALSE
+  return(same)
+}
+
+################# FAJLBEOLVASAS ##################################
+# Egy adott k?nyvt?r 4-13. (csv) f?jljait ?sszefuzz?k egy nagy dataframe-be
+# ?s m?g hozz?adunk sorfolytonos id ?s olyan attrib?tumot timestamp alapj?n, hogy mely ?r?ban t?rt?nt megfigyel?s
 filenames <- list.files(path = "D:Downloads/bigdata_data", full.names = TRUE)
 filenames
 d0 <- do.call("rbind", lapply(filenames[4:13], read.csv, col.names = c('ts','line_id','direction','journey_pattern_id','time_frame','vehicle_journey_id','operator','congestion','lon','lat','delay','block_id','vehicle_id','stop_id','at_stop')))
-d0$id <- 1:nrow(d0)
-d0$tst <- paste(format(as.POSIXct(d0$ts/1e6, origin="1970-01-01"),"%Y-%b-%d %H"),'h', sep='')
 
 # fajl beolvasas TODO: egyelore csak egy nap
-d0 = read.csv('e:/munka/BME/BigData/siri.20121125.csv',col.names = c('ts','line_id','direction','journey_pattern_id','time_frame','vehicle_journey_id','operator','congestion','lon','lat','delay','block_id','vehicle_id','stop_id','at_stop'))
+#d0 = read.csv('e:/munka/BME/BigData/siri.20121125.csv',col.names = c('ts','line_id','direction','journey_pattern_id','time_frame','vehicle_journey_id','operator','congestion','lon','lat','delay','block_id','vehicle_id','stop_id','at_stop'))
 #subset(d0[ ! duplicated( d0[ c("line_id","operator") ] ) , ], select=c("line_id","operator"))
 #summary(d0)
-d0 = d0[order(d0$time_frame,d0$vehicle_journey_id, d0$ts, d0$journey_pattern_id),] 
+
+# megfelelo modon sorrendezzuk a rekordokat
+d0 = d0[order(d0$time_frame,d0$vehicle_journey_id, d0$ts, d0$journey_pattern_id),]
+
+#sorazonosito generalasa
 d0$id <- 1:nrow(d0)
+
+d#az idobeli aggregacio alapja, melyik nap melyik orajarol van szÃ³
 d0$tst <- paste(format(as.POSIXct(d0$ts/1e6, origin="1970-01-01"),"%Y-%b-%d %H"),'h', sep='')
 
+########### HAVERSINE TAVOLSAG ##################
+# haversine tavolsagok szamitasa mapreduce nelkul
+# az MR11-es mapreduce kivaltasara
+#################################################
+# Agi javaslatara mapreduce nelkul is megcsinaltuk a tavolsagszamitast
+
+# oszlopok eltolasa
 d0 <- shift.column(data=d0, columns=c("lat","lon","vehicle_journey_id"),up=FALSE)
 
 calc_haversine <- function(lat, lon, plat, plon, vjid, pvjid) {
+  # radian konverzio
   rlat = lat*pi/180
   rlon = lon*pi/180
   rplat = plat*pi/180
@@ -46,42 +70,44 @@ calc_haversine <- function(lat, lon, plat, plon, vjid, pvjid) {
   a <- sin(delta.lat/2)^2 + cos(rlat) * cos(rplat) * sin(delta.long/2)^2
   c <- 2 * asin(min(1,sqrt(a)))
   
+  # csak ertelmes esetekben adjuk vissza a kalkulalt erteket
   if (compareNA(vjid,pvjid)) distdelta = R*c
   else distdelta = NA
   return(distdelta)
 }
 
-
+# tavolsag szamitasa minden sorra
 d0$distdelta <- mapply(calc_haversine, d0$lat, d0$lon, d0$lat.Shifted, d0$lon.Shifted, d0$vehicle_journey_id, d0$vehicle_journey_id.Shifted) 
  
   
-#deleting columns
+# tovabbiakban felesleges oszlopok torlese, takaritas
 d0 <-subset(d0,,-c(lon.Shifted,lat.Shifted,vehicle_journey_id.Shifted))
 
 # lenyomjuk az adatot "hadoop"-ba
 hd0 <- to.dfs(d0)
 
 
-#csinalunk egy pici mintat
-td0 <- to.dfs(head(d0,500))
+#csinalunk egy pici mintat, csak teszteleshez
+#td0 <- to.dfs(head(d0,500))
 
-# MAPREDUCE kódok
+############# MAPREDUCE kodok###################
+################################################
 
-# utolsó mért késés járatra max timestamp alapján
+# MR1 utols? m?rt k?s?s j?ratra max timestamp alapj?n
 end_delay_per_journey <-  mapreduce(input = hd0, 
                                     map = function(., v){
                                       keyval(v[, c("vehicle_journey_id","time_frame")], v[, c("delay","ts")])
                                     },
                                     reduce = function(k, vv) {
-                                      #kikeressük max timestamp-hez a késés mértékét
+                                      #kikeress?k max timestamp-hez a k?s?s m?rt?k?t
                                       max_place <- which.max(vv[, c("ts")])
                                       last_delay <- vv[max_place, c("delay")]
-                                      # csak azt engedjük tovább, ami nem 0 késés, sanszos, hogy az rossz adat (sok járatnál végig 0)
+                                      # csak azt engedj?k tov?bb, ami nem 0 k?s?s, sanszos, hogy az rossz adat (sok j?ratn?l v?gig 0)
                                       if(last_delay!=0)keyval(k, last_delay)
                                     }
 )
 
-# járatok medián késése naponként
+# MR2 j?ratok medi?n k?s?se naponk?nt
 avg_delay_per_day <-  mapreduce(input = end_delay_per_journey, 
                                 map = function(k, v){
                                   # A nap lesz a kulcs
@@ -98,13 +124,13 @@ daily <- data.frame(days=from.dfs(avg_delay_per_day)$key, delay=from.dfs(avg_del
 daily$days <- factor(daily$days, levels= c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"))
 daily <- daily[order(daily$days), ]
 
-# Ehhez tartozó plot (lattice kell hozzá)
+# Ehhez tartoz? plot (lattice kell hozz?)
 plt <- xyplot(daily$delay ~ daily$days, type='b', xlab="Days", ylab="Delay")
 update(plt, par.settings = list(fontsize = list(text = 25, points = 20)))
 
 # -----------------------------------------------------------------------------------
 
-# egyedi line, operator parok kinyerese trukkos aggregacioval TODO: lehetne szebb
+# MR3 egyedi line, operator parok kinyerese trukkos aggregacioval
 hlines_ops <-  mapreduce(input = hd0, 
                          map = function(., v)
                            keyval(v[, c("line_id","operator")], 1),
@@ -112,7 +138,7 @@ hlines_ops <-  mapreduce(input = hd0,
                            keyval(k, 1)
                          }
 )
-# operatorok szama vonalankent
+# MR4.1 operatorok szama vonalankent
 hops_per_line <-  mapreduce(input = hlines_ops, 
                             map = function(k, .)
                               keyval(k[, c("line_id")], 1),
@@ -120,13 +146,14 @@ hops_per_line <-  mapreduce(input = hlines_ops,
                               keyval(k, length(vv))
                             }
 )
+# MR4.2 peratorok szama vonalankent alternativ megoldas
 hops_per_line <- mapreduce(hlines_ops, 
                              map = function(k, v)
                                keyval(k$line_id, 1), 
                              reduce = function(k, v) 
                                cbind(line = k, no_ops = sum(v, na.rm = TRUE)))
 
-# vonalak szama operatoronkent
+# MR5 vonalak szama operatoronkent
 hlines_per_op <-  mapreduce(input = hlines_ops, 
                             map = function(k, .)
                               keyval(k[, c("operator")], 1),
@@ -134,7 +161,7 @@ hlines_per_op <-  mapreduce(input = hlines_ops,
                               keyval(k, length(vv))
                             }
 )
-# atlagos keses vonalankent
+# MR6.1 atlagos keses vonalankent
 hdelay_per_line <-  mapreduce(input = hd0, 
                               map = function(., v)
                                 keyval(v[, c("line_id")],v[, c("delay")]),
@@ -143,7 +170,7 @@ hdelay_per_line <-  mapreduce(input = hd0,
                               }
 )
 
-# atlagos keses vonalankent 2. szamu megoldas
+# MR6.2 atlagos keses vonalankent 2. szamu megoldas
 
 hdelay_per_line <- mapreduce(hd0, 
     map = function(k, v)
@@ -152,7 +179,7 @@ hdelay_per_line <- mapreduce(hd0,
         cbind(line = k, mean = mean(v, na.rm = TRUE)))
 rread(hdelay_per_line,FALSE)
 
-# Eredmények kinyerése
+# Eredm?nyek kinyer?se
 result <- from.dfs(hdelay_per_line)
 d1 = data.frame(result)
 colnames(d1) <- c('line_id','mean_delay')
@@ -161,16 +188,17 @@ result <- from.dfs(hops_per_line)
 d2 = data.frame(result)
 colnames(d2) <- c('line_id','operator')
 
+# tablak kombinalasa
 d3 = merge(x = d1, y = d2, by = "line_id", all = TRUE)
 
 # Ehhez plotok
 
-# Operátorok és késések scatter plot
+# Oper?torok ?s k?s?sek scatter plot
 p1 <- ggplot(d3, aes(x = operator, y = mean_delay))
 p1 <- p1 + geom_point(color="blue", size = 4)
 p1 + theme(axis.title=element_text(face="bold",size="20"), axis.text = element_text(size = 20), legend.position="top") + labs(x = "Number of Operators", y = "Delay")
 
-# Operátorszám és irányított vonalszám eloszlás
+# Oper?torsz?m ?s ir?ny?tott vonalsz?m eloszl?s
 bar <- barchart(d2$operator, horizontal=FALSE, xlab="Number of Operators", ylab="Number of Lines")
 update(bar, par.settings = list(fontsize = list(text = 20)))
 
@@ -178,10 +206,7 @@ update(bar, par.settings = list(fontsize = list(text = 20)))
 barplot(height=from.dfs(hlines_per_op)$val, names.arg=from.dfs(hlines_per_op)$key, xlab="Operators", ylab="Lines")
 
 
-# -----------------------------------------------------------------------------------------------------
-
-# INNEN KELLENE FUTTATUNI
-# szumma atlagos keses orankent
+# MR7 atlagos keses orankent
 hhourly_meandelay <-  mapreduce(input = hd0, 
                                 map = function(., v)
                                   keyval(v[, c("vehicle_journey_id","time_frame","tst")], v[, c("delay")]),
@@ -192,6 +217,7 @@ hhourly_meandelay <-  mapreduce(input = hd0,
 
 #rread(hhourly_meandelay)
 
+# MR8 szumma atlagos keses orankent
 hhourly_totaldelay <-  mapreduce(input = hhourly_meandelay, 
                                 map = function(k, v)
                                   keyval(k[, c("tst")], v),
@@ -215,9 +241,9 @@ ddelay = ddelay[order(ddelay$hour),]
 # Ehhez plot
 plot <- ggplot( data = ddelay, aes( strptime(hour, "%Y-%B-%d %Hh"), total_delay /3600 )) + geom_line()
 plot + theme(axis.title=element_text(face="bold",size="20"), axis.text = element_text(size = 20), legend.position="top") + labs(x = "Time (Hours)", y = "Delay (Hours)")
-# FUTTATAS VEGE
 
-# szumma km orankent
+
+# MR9 szumma km orankent
 
 hhourly_totaldist <-  mapreduce(input = hd0, 
                                 map = function(., v)
@@ -231,12 +257,11 @@ dkm = data.frame(from.dfs(hhourly_totaldist))
 colnames(dkm) <- c('tst','distdelta')
 dkm = dkm[order(dkm$tst),]
 
-# Plot az óránkénti összes kilométerhez
+# Plot az ?r?nk?nti ?sszes kilom?terhez
 plot <- ggplot( data = dkm, aes( strptime(tst, "%Y-%B-%d %Hh"), distdelta)) + geom_line()
 plot + theme(axis.title=element_text(face="bold",size="20"), axis.text = element_text(size = 20), legend.position="top") + labs(x = "Time (Hours)", y = "Total distance (km)")
 
-# szumma jarmuvek orankent
-
+# MR10 szumma jarmuvek orankent
 hhourly_count <-  mapreduce(input = hd0, 
                                 map = function(., v)
                                   keyval(v[, c("tst")], v[, c("vehicle_id")]),
@@ -249,32 +274,13 @@ dcount = data.frame(from.dfs(hhourly_count))
 colnames(dcount) <- c('tst','vehicle_count')
 dcount = dcount[order(dcount$tst),]
 
-# Plot óránkénti jármumennyiséghez
+# Plot ?r?nk?nti j?rmumennyis?ghez
 plot2 <- ggplot( data = dcount, aes( strptime(tst, "%Y-%B-%d %Hh"), vehicle_count)) + geom_line()
 plot2 + theme(axis.title=element_text(face="bold",size="20"), axis.text = element_text(size = 20), legend.position="top") + labs(x = "Time (Hours)", y = "Number of Vehicles on Journey")
 
-# EDDIG
 
-# haversine tavolsag
-# TODO: rendezes, vehicle valtasnal torles, beletenni az utolso ismert delay-t 
 
-compareNA <- function(v1,v2) {
-  # This function returns TRUE wherever elements are the same, including NA's,
-  # and false everywhere else.
-  same <- (v1 == v2)  |  (is.na(v1) & is.na(v2))
-  same[is.na(same)] <- FALSE
-  return(same)
-}
-
-# hordered <- mapreduce(td0, 
-#                              map = function(k, v)
-#                                keyval(v$vehicle_journey_id, v), 
-#                              reduce = function(k, v) 
-#                                keyval(k,v))
-# rread(hordered)
-# 
-# result$id <- 1:nrow(result)
-
+# MR11 haversine tavolsag trukkos mapreduce-szal
 haversines <- mapreduce(hd0, 
                         map = function(k, v)
                           keyval(c(v$id,v$id+1),cbind(v$lat,v$lon,v$vehicle_journey_id,v$delay)),   
@@ -318,53 +324,24 @@ haversines <- mapreduce(hd0,
 
 rread(haversines)
 
-hhourly_totalkm <-  mapreduce(input = haversines, 
-                                 map = function(., v)
-                                   keyval(v[, c("vjid")], v[, c("distdelta")]),
-                                 reduce = function(k, vv) 
-                                   keyval(k, sum(vv, na.rm = TRUE)) 
-)
 
 
-rread(hhourly_totalkm,FALSE)
 
-result <- from.dfs(haversines)
-dhaver = data.frame(result)
-
-# eredmenyek kiolvasasa
-from.dfs(hdelay_per_line)
-result <- from.dfs(hdelay_per_line)
-d1 = data.frame(result)
-colnames(d1) <- c('line_id','mean_delay')
-
-#from.dfs(hlines_per_op)
-from.dfs(hops_per_line)
-result <- from.dfs(hops_per_line)
-d2 = data.frame(result)
-colnames(d2) <- c('line_id','operator')
-
-# plotolas
-d3 = merge(x = d1, y = d2, by = "line_id", all = TRUE)
-plot(d3$mean_delay, d3$operator)
-# szebben
-p1 <- ggplot(d3, aes(x = operator, y = mean_delay))
-p2 <- p1 + geom_point(color="blue")            #set one color for all points
-
-
-# MAP plotok ------------------------------------------------------------------------
+#################### TERKEPES plotok ################
+#####################################################
 
 # sima plot
 DublinMap <- qmap('dublin', zoom = 11,color = 'bw', legend = 'topleft')
 DublinMap +geom_point(aes(x = lon, y = lat), data = subset(d0, at_stop == 1) )
 
-# map line id alapján
+# map line id alapj?n
 DublinMap <- qmap('dublin', zoom = 11,color = 'bw', legend = 'topleft')
 DublinMap +geom_point(aes(x = lon, y = lat), size = 4, data = subset(d0, line_id == "54A") )
 
-# map line id alapján, zoomolva
+# map line id alapj?n, zoomolva
 DublinMap <- qmap('dublin', zoom = 15,color = 'bw', legend = 'topleft')
 DublinMap +geom_point(aes(x = lon, y = lat), size = 4, data = subset(d0, line_id == "54A") )
 
-# map operator alapján színezve
+# map operator alapj?n sz?nezve
 DublinMap <- qmap('dublin', zoom = 11,color = 'bw', legend = 'topleft')
 DublinMap +geom_point(aes(x = lon, y = lat, colour = operator), data = subset(d0, at_stop == 1) )
